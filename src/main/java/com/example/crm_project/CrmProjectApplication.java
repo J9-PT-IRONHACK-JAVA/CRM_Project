7b13bd0bfaa1a7757eca394b13ffac7a11daf5e4
andresagdt515 <83793367+andresagdt515@users.noreply.github.com>
2022-11-17 14:39:45 +0100

    structure

--- src/main/java/com/example/crm_project/CrmProjectApplication.java
+++ src/main/java/com/example/crm_project/CrmProjectApplication.java
@@ -1,13 +1,20 @@
 package com.example.crm_project;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class CrmProjectApplication {
+public class CrmProjectApplication implements CommandLineRunner {
 
 	public static void main(String[] args) {
 		SpringApplication.run(CrmProjectApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		System.out.println("Hello i'm running!");
 	}
 
 }
