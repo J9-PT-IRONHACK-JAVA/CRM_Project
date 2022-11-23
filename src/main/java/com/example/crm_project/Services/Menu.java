23d0ad3da68fb6de8631b385e39988fabf62d893
andresagdt515 <83793367+andresagdt515@users.noreply.github.com>
2022-11-23 23:28:02 +0100

    improvement_InputService_unfinished

--- src/main/java/com/example/crm_project/Services/Menu.java
+++ src/main/java/com/example/crm_project/Services/Menu.java
@@ -2,10 +2,26 @@ package com.example.crm_project.Services;
 
 import com.example.crm_project.utils.Commands;
 
+import java.util.Scanner;
+
+import static com.example.crm_project.Services.InputService.EXIT;
+import static com.example.crm_project.Services.InputService.YES;
+import static com.example.crm_project.utils.Prints.exitApp;
 import static com.example.crm_project.utils.TypeWriter.typewriterFromString;
 
 public class Menu {
+    private static InputService inputSVC;
     public static void mainMenu() {
-        typewriterFromString(Commands.MenuMessage, 0);
+
+        String input;
+        do {
+            typewriterFromString(Commands.MenuMessage, 0);
+            input = new Scanner(System.in).nextLine();
+            inputSVC.executeCommand(input);
+
+        } while (input.equals(EXIT));
+
+        exitApp();
+        System.exit(0);
     }
 }
