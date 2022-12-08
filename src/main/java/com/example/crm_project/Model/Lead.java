7ddadb2f480671eb5b284d9a6ece0681d120895a
paniaguaadrian <paniaguasanchezadrian@gmail.com>
2022-12-08 04:42:05 +0100

    add queries and tests for account and salesRep

--- src/main/java/com/example/crm_project/Model/Lead.java
+++ src/main/java/com/example/crm_project/Model/Lead.java
@@ -14,7 +14,7 @@ import javax.validation.constraints.NotNull;
 public class Lead {
 
     @Id
-    @NotNull
+    //@NotNull
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
